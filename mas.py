ad9cfa74c35ac9418120b2af2370f608ac4dc1de
ngshya <y@shuyi.it>
2025-01-11 19:08:06 -0800

    Cleaning code.

--- mas.py
+++ mas.py
@@ -2,8 +2,7 @@ from warnings import filterwarnings
 filterwarnings("ignore", message="flaml.automl is not available.*")
 
 import os
-from autogen import (
-    Agent, 
+from autogen import ( # type: ignore
     ConversableAgent, 
     GroupChat, 
     GroupChatManager, 
@@ -11,7 +10,7 @@ from autogen import (
 )
 from datetime import datetime, timezone
 from json import dump
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 from prompts import (
     SYSTEM_PROMPT_AGENT_INGESTION, DESCRIPTION_AGENT_INGESTION, 
     SYSTEM_PROMPT_AGENT_RETRIEVE, DESCRIPTION_AGENT_RETRIEVE, 
@@ -22,18 +21,26 @@ from tools_ingestion import path_to_db, text_to_db
 from tools_retrieve import retrieve_str
 
 
-load_dotenv()
+#####################
+### Configuration ###
+#####################
 
+load_dotenv()
 llm_config = {
     "config_list": [
         {
-            "model": "gpt-4o-mini", 
+            "model": "gpt-4o", 
             "api_key": os.environ["OPENAI_API_KEY"], 
             "temperature": 0.7,
-            "cache_seed": None
+            # "cache_seed": None
         }
     ]
 }
+
+
+#########################
+### Agents definition ###
+#########################
 
 human = ConversableAgent(
     name = "human",
@@ -48,22 +55,7 @@ agent_ingestion = ConversableAgent(
     description = DESCRIPTION_AGENT_INGESTION,
     llm_config = llm_config,
     human_input_mode = "NEVER",
-)
-
-register_function(
-    path_to_db,
-    caller=agent_ingestion,  
-    executor=agent_ingestion,  
-    name="path_to_db",  
-    description="Ingest new knowledge from a text file given its path.",
-)
-
-register_function(
-    text_to_db,
-    caller=agent_ingestion,  
-    executor=agent_ingestion,  
-    name="text_to_db",  
-    description="Ingest new knowledge from a piece of conversation.",
+    silent=False
 )
 
 agent_retrieve = ConversableAgent(
@@ -72,14 +64,7 @@ agent_retrieve = ConversableAgent(
     description = DESCRIPTION_AGENT_RETRIEVE,
     llm_config = llm_config,
     human_input_mode = "NEVER",
-)
-
-register_function(
-    retrieve_str,
-    caller=agent_retrieve,  
-    executor=agent_retrieve,  
-    name="retrieve_str",  
-    description="Retrieve useful information from internal DB.",
+    silent=False
 )
 
 agent_answer = ConversableAgent(
@@ -88,6 +73,7 @@ agent_answer = ConversableAgent(
     description = DESCRIPTION_AGENT_ANSWER,
     llm_config = llm_config,
     human_input_mode = "NEVER",
+    silent=False
 )
 
 agent_router = ConversableAgent(
@@ -98,9 +84,42 @@ agent_router = ConversableAgent(
     human_input_mode = "NEVER",
 )
 
+
+####################
+### Adding tools ###
+####################
+
+register_function(
+    path_to_db,
+    caller=agent_ingestion,  
+    executor=agent_ingestion,  
+    name="path_to_db",  
+    description="Ingest new knowledge from a text file given its path.", 
+)
+
+register_function(
+    text_to_db,
+    caller=agent_ingestion,  
+    executor=agent_ingestion,  
+    name="text_to_db",  
+    description="Ingest new knowledge from a piece of conversation.",
+)
+
+register_function(
+    retrieve_str,
+    caller=agent_retrieve,  
+    executor=agent_retrieve,  
+    name="retrieve_str",  
+    description="Retrieve useful information from internal DB.",
+)
+
+
+#########################
+### Nested group chat ###
+#########################
+
 group_chat = GroupChat(
     agents = [
-        human, 
         agent_router,
         agent_ingestion, 
         agent_retrieve, 
@@ -108,31 +127,52 @@ group_chat = GroupChat(
     ],
     messages=[],
     send_introductions=False,
-    max_round=100,
+    max_round=10,
     speaker_selection_method="auto", 
     speaker_transitions_type="allowed", 
     allowed_or_disallowed_speaker_transitions={
-        human: [agent_router],
-        agent_router: [human, agent_ingestion, agent_retrieve, agent_answer],
+        agent_router: [agent_ingestion, agent_retrieve, agent_answer],
         agent_ingestion: [agent_router],
         agent_retrieve: [agent_answer],
         agent_answer: [agent_router],
-    }
+    }, 
 )
 
 group_chat_manager = GroupChatManager(
     groupchat=group_chat,
     llm_config=llm_config, 
+    silent=False, 
+    is_termination_msg=lambda msg: "(to human)" in msg["content"].lower()
+)
+
+nested_chats = [
+    {
+        "recipient": group_chat_manager,
+        "summary_method": "last_msg",
+    }
+]
+
+agent_router.register_nested_chats(
+    nested_chats,
+    trigger=lambda sender: sender in [human],
 )
 
 
+################################################################################
+################################################################################
+################################################################################
+
+
 if __name__ == "__main__":
-    msg = input("You initial query: ")
-    chat_results = human.initiate_chat(
-        group_chat_manager, 
-        message=msg, 
-        summary_method="last_msg",
+    chat_results = agent_router.initiate_chat(
+        human, 
+        message="Ciao! How can I assist you today? 😊", 
+        max_turns = 100
     )
     tms_now = datetime.now(timezone.utc).strftime("%d_%B_%Y_%H_%M_%S_%Z_%z")
     with open("chat_logs/%s.json" % (tms_now), "w") as f:
-        dump(chat_results.chat_history, f, indent=4)
+        dump({
+            "chat_history": chat_results.chat_history, 
+            "nested_chat": list(group_chat_manager.chat_messages.values())[-1]
+        }, f, indent=4)
+
